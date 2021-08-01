e15da9603ebf672d55f2b06b534c7bd81dd86c29
Teodoro Alcántara Aquino <teodoro641@hotmail.com>
2021-08-01 07:38:45 -0400

    Added Sum of odd elements haskell solution

--- haskell/ReverseAList/ReverseAList.hs
+++ haskell/ReverseAList/ReverseAList.hs
@@ -1,5 +1,5 @@
 rev :: [a] -> [a]
 rev [] = []
-rev xs = inv $ (length xs) - 1
-  where inv 0 = [xs !! 0]
+rev xs = inv $ length xs - 1
+  where inv 0 = [head xs]
         inv n = (xs !! n) : inv(n - 1)
